c0e56aaa9f18fcdc2d662550e58f324b7d8c587a
tlin108 <tlin108@hotmail.com>
2016-12-27 23:33:40 -0500

    consuming actions in reducers

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,7 @@
 export function selectBook(book) {
-  console.log('A Book has been selected: ', book.title);
+  // selectBook is an ActionCreator, it needs to return an action, an object with a type property.
+  return {
+    type: 'BOOK_SELECTED',
+    payload: book
+  };
 }
